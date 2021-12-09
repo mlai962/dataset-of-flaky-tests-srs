ffa8ba5c0b12245c739125afdb8766b2eec9c1e3
Matthew <mlai962@aucklanduni.ac.nz>
2021-12-09 18:13:57 +1300

    Deleted automatically generated class from the maven build and tested a simple search for issues with the keyword flaky

--- src/main/java/flakyTestSearch/main.java
+++ src/main/java/flakyTestSearch/main.java
@@ -5,25 +5,13 @@ import kong.unirest.Unirest;
 import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
 
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+
 public class main {
-
 	public static void main (String args[]) {
-		kong.unirest.HttpResponse<JsonNode> jsonResponse
-		= Unirest.get("https://api.github.com/search/issues")
-		.basicAuth("mlai962", "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")
-		.queryString("q", "flaky")
-		.queryString("page", 1)
-		.queryString("per_page", 100)
-		.asJson();
-
-		System.out.println(jsonResponse.getBody().toPrettyString());
-
-		JSONObject j = jsonResponse.getBody().getObject();
-		JSONArray jsonarray = j.getJSONArray("items");
-		for (int i = 0; i < jsonarray.length(); i++) {
-			JSONObject obj = jsonarray.getJSONObject(i);
-			System.out.println(obj.get("repository_url"));
-			//		System.out.println(obj.get("body"));
-		}
+		searchGitHub.findFlakyness();
 	}
 }
