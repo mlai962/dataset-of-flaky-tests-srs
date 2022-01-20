4328fab5e1a7cfdeb5ebd17956bd107ce6dcfb99
Matthew <mlai962@aucklanduni.ac.nz>
2022-01-20 13:06:19 +1300

    Added dependencies to get rid of warnings occuring when calling console commands. RepoUtil now correctly clones repos and checks for the test class existing.

--- src/main/java/flakyTestSearch/RepoUtil.java
+++ src/main/java/flakyTestSearch/RepoUtil.java
@@ -1,40 +1,108 @@
 package flakyTestSearch;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.nio.file.Paths;
-
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.eclipse.jgit.util.FileUtils;
 
 public class RepoUtil {
-	public static void deleteTempRepoDir() {
-		String dir = "C:\\Users\\Matthew\\Downloads\\Documents\\School 2021\\tempRepo";
+	private static String dir = "C:\\Users\\Matthew\\Downloads\\Documents\\School 2021\\tempRepo";
+	private static String lastRepoName = "";
+
+	// ****** https://mkyong.com/java/how-to-execute-shell-command-from-java/ ***** 
+	public static boolean executeCommand(String cmd, String directory) {
+		boolean isExecuted = false;
+
 		try {
-			FileUtils.delete(new File(dir), org.eclipse.jgit.util.FileUtils.RECURSIVE);
+			ProcessBuilder processBuilder = new ProcessBuilder();
+
+			processBuilder.command("cmd.exe", "/c", cmd).directory(new File(directory));
+
+			Process process = processBuilder.start();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+			String errorLine;
+			while ((errorLine = error.readLine()) != null) {
+				System.out.print(errorLine);
+			}
+
+			int exitVal = process.waitFor();
+			if (exitVal == 0) {
+				isExecuted = true;
+				System.out.println("Success!");
+			} else {
+				System.out.println("Fail!");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return isExecuted;
+	}
+
+	public static void deleteTempRepoDir() {
+		try {
+			FileUtils.delete(new File(dir + File.separator + lastRepoName + File.separator), org.eclipse.jgit.util.FileUtils.RECURSIVE);
 		} catch (java.io.IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static Boolean cloneRepo (Project project) {
-		Boolean isCloned = false;
+	public static boolean cloneRepo (Project project) {
+		if (!lastRepoName.equals("")) {
+			deleteTempRepoDir();
+		}
 
 		String repoURL = project.getProjectName();
-		String directoryPath = "C:\\Users\\Matthew\\Downloads\\Documents\\School 2021\\tempRepo";
-		
-		try {
-		    Git.cloneRepository()
-		        .setURI(repoURL)
-		        .setDirectory(Paths.get(directoryPath).toFile())
-		        .call();
-		    
-		    isCloned = true;
-		} catch (GitAPIException e) {
-		    System.out.println("Exception occurred while cloning repo");
-		    e.printStackTrace();
+		String cloneURL = repoURL.replace("api", "www").replace("repos/", "") + ".git/";
+
+		String commitHash = project.getCommitHash();
+
+		boolean isCloned = executeCommand("git clone " + cloneURL, dir);
+
+		if (isCloned) {
+			int indexOfLastSlashRepoName = project.getProjectName().lastIndexOf("/");
+			String repoName = project.getProjectName().substring(indexOfLastSlashRepoName+1, project.getProjectName().length());
+			lastRepoName = repoName;
+			
+			return executeCommand("git reset " + commitHash + " --hard", dir + File.separator + repoName);
+		} else {
+			return false;
 		}
+	}
+
+	public static Boolean checkClassExists (Project project) {
+		int indexOfLastSlashClassName = project.getClassName().lastIndexOf("/");
+		int indexOfLastSlashRepoName = project.getProjectName().lastIndexOf("/");
+		String repoName = project.getProjectName().substring(indexOfLastSlashRepoName+1, project.getProjectName().length());
+		File testClassDir = new File(dir + File.separator + repoName + File.separator + project.getClassName().substring(0, indexOfLastSlashClassName+1) + File.separator);
+		final String testClassName = project.getClassName().substring(indexOfLastSlashClassName+1, project.getClassName().length());
 		
-		return isCloned;
+		System.out.println(testClassName);
+		System.out.println(testClassDir);
+
+		File[] matches = testClassDir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith(testClassName);
+			}
+		});
+
+		if (!(matches == null)) {
+			System.out.println(matches[0]);
+			return true;
+		}
+
+		return false;
 	}
 }
