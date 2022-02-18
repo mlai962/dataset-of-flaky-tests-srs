b678e5c2b76b48be8051a0ca8813f9189017555c
Matthew <mlai962@aucklanduni.ac.nz>
2022-02-18 14:58:26 +1300

    Added analyseSingleTestOutput method to TestFlakyness class that checks the output when running a single test. Removed the output analysis from executeCommand method.

--- src/main/java/flakyTestSearch/TestFlakyness.java
+++ src/main/java/flakyTestSearch/TestFlakyness.java
@@ -7,63 +7,74 @@ import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
 public class TestFlakyness {
-	// Method below taken from https://mkyong.com/java/how-to-execute-shell-command-from-java/
-		public static int executeCommand(String cmd, String directory) {
-			int returnValue = 1;
-			int exitValue = 0;
-			
-			try {
-				ProcessBuilder processBuilder = new ProcessBuilder();
+	public static int analyseSingleTestOutput(BufferedReader reader) {
+		int returnValue = 1;
+		String line;
 
-				processBuilder.command("bash", "-l", "-c", cmd).directory(new File(directory));
-
-				Process process = processBuilder.start();
-				
-				exitValue = process.waitFor();
-				
-				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-				BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-
-				String line;
-				while ((line = reader.readLine()) != null) {
-					System.out.println(line);
-					if (line.contains("Failures: 1") || line.contains("Errors: 1")) {
-						returnValue = 0;
-					} else if (line.contains("BUILD FAILURE")) {
-						returnValue = 2;
-					}
-					
-				}
-
-				String errorLine;
-				while ((errorLine = error.readLine()) != null) {
-					System.out.println(errorLine);
-				}
-				
-				if (exitValue != 0) {
-					process.destroy();
-					
-					System.out.println("command execution failure");
-					
+		try {
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+				if (line.contains("Failures: 1") || line.contains("Errors: 1")) {
+					returnValue = 0;
+				} else if (line.contains("BUILD FAILURE")) {
 					returnValue = 2;
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			} 
-			
-			return returnValue;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-	
+
+		return returnValue;
+	}
+
+	// Method below taken from https://mkyong.com/java/how-to-execute-shell-command-from-java/
+	public static int executeCommand(String cmd, String directory) {
+		int returnValue = 1;
+		int exitValue = 0;
+
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder();
+
+			processBuilder.command("bash", "-l", "-c", cmd).directory(new File(directory));
+
+			Process process = processBuilder.start();
+
+			exitValue = process.waitFor();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+			returnValue = analyseSingleTestOutput(reader);
+
+			String errorLine;
+			while ((errorLine = error.readLine()) != null) {
+				System.out.println(errorLine);
+			}
+
+			if (exitValue != 0) {
+				process.destroy();
+
+				System.out.println("command execution failure");
+
+				returnValue = 2;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+
+		return returnValue;
+	}
+
 	public static TestResult runSingleTest(Project project, String className, String testName, int numOfRuns, boolean isMaven, boolean hasWrapper) {
 		double failures = 0;
 		double flakyness = 0;
-		
+
 		int lastSlash = className.lastIndexOf("/");
 		className = className.substring(lastSlash+1, className.length());
-		
+
 		String dir = Config.TEMP_DIR + File.separator + project.getProjectName();
 		String cmd = "";
-		
+
 		if (isMaven && hasWrapper) {
 			cmd = "./mvnw test -Dtest=" + className + "#" + testName;
 		} else if (isMaven) {
@@ -73,19 +84,19 @@ public class TestFlakyness {
 		} else {
 			cmd = "gradle test --tests " + className + "." + testName + " -i";
 		}
-		
+
 		for (int i = 0; i < numOfRuns; i++) {
 			int result = executeCommand(cmd, dir);
-			
+
 			if (result == 0) {
 				failures++;
 			} else if (result == 2) {
 				return new TestResult(className, testName, 0, false, true);
 			}
 		}
-		
+
 		flakyness = failures / numOfRuns;
-		
+
 		return new TestResult(className, testName, flakyness, false, false);
 	}
 }
